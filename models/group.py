8557d8316d9fc13044f6b0f88d95e340fce82e47
sabinshrestha2003 <np03cs4a220061@heraldcollege.edu.np>
2025-05-07 18:41:15 +0545

    update

--- models/group.py
+++ models/group.py
@@ -5,17 +5,18 @@ class Group(db.Model):
     __tablename__ = 'groups'
 
     my_row_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    id = db.Column(db.Integer, nullable=False)  
+    id = db.Column(db.Integer, nullable=False, unique=True)  
     name = db.Column(db.String(100), nullable=False)
     creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     type = db.Column(db.String(20), nullable=False, default='Custom')
     currency = db.Column(db.String(10), nullable=True, default='INR')
     created_at = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
     icon_url = db.Column(db.String(255), nullable=True)
+    deleted_at = db.Column(db.DateTime, nullable=True, default=None)
 
     members = db.relationship('GroupMember', backref='group', lazy=True, cascade='all, delete')
 
-    def __init__(self, id, name, creator_id, type='Custom', currency='USD', created_at=None, icon_url=None):
+    def __init__(self, id, name, creator_id, type='Custom', currency='USD', created_at=None, icon_url=None, deleted_at=None):
         self.id = id
         self.name = name
         self.creator_id = creator_id
@@ -23,10 +24,10 @@ class Group(db.Model):
         self.currency = currency
         self.created_at = created_at or datetime.utcnow()
         self.icon_url = icon_url
+        self.deleted_at = deleted_at
 
     def to_dict(self):
         return {
-            'my_row_id': self.my_row_id, 
             'id': self.id,
             'name': self.name,
             'creator_id': self.creator_id,
